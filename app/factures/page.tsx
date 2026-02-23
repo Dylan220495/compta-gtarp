9789142cfaef28602787be84625d2e0b254e7933
Dylan <kalash220495@gmail.com>
2026-02-23 15:40:24 +0100

    Ajout page factures

---
+++ app/factures/page.tsx
@@ -0,0 +1,8 @@
+export default function Factures() {
+  return (
+    <main style={{ padding: "40px", fontFamily: "Arial" }}>
+      <h1>📄 Liste des Factures</h1>
+      <p>Voici la page des factures.</p>
+    </main>
+  );
+}
